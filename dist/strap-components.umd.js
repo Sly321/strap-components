96a7b2e2ebb40d50f3808dfdf621a6068985a926
Sven Liebig <liebigsv@gmail.com>
2019-08-07 20:49:02 +0000

    docs(docs): update gh-pages

---
+++ dist/strap-components.umd.js
@@ -0,0 +1,51 @@
+(function (factory) {
+    typeof define === 'function' && define.amd ? define(factory) :
+    factory();
+}(function () { 'use strict';
+
+    class Button extends HTMLElement {
+        constructor() {
+            super();
+            this.text = '';
+            this.properties = {};
+            this.text = this.innerHTML;
+            for (let x = 0; x < this.attributes.length; x++) {
+                const attr = this.attributes.item(x);
+                if (attr) {
+                    this.properties[attr.name] = attr.value;
+                }
+            }
+            this.innerHTML = this.render();
+        }
+        // classNames
+        get clx() {
+            const rootClasses = this.properties.class;
+            delete this.properties.class;
+            return `btn btn-${this.outline ? 'outline-' : ''}${this.color}${rootClasses ? ` ${rootClasses}` : ''}${this.block ? ' btn-block' : ''}`;
+        }
+        get color() {
+            delete this.properties.color;
+            return (this.hasAttribute('color') && this.getAttribute('color')) || 'primary';
+        }
+        get outline() {
+            delete this.properties.outline;
+            return this.hasAttribute('outline') && this.getAttribute('outline') !== 'false';
+        }
+        get block() {
+            delete this.properties.outline;
+            return this.hasAttribute('block') && this.getAttribute('block') !== 'false';
+        }
+        render() {
+            return `<button class="${this.clx}" ${Object.keys(this.properties)
+            .map(key => {
+            return `${key}="${this.properties[key]}"`;
+        })
+            .join(' ')}>
+			${this.text}
+		</button>`;
+        }
+    }
+    customElements.define('bs-button', Button);
+
+}));
+//# sourceMappingURL=strap-components.umd.js.map
---
+++ dist/types/Button.d.ts
@@ -0,0 +1,10 @@
+export default class Button extends HTMLElement {
+    private text;
+    private properties;
+    constructor();
+    readonly clx: string;
+    readonly color: string;
+    readonly outline: boolean;
+    readonly block: boolean;
+    render(): string;
+}
